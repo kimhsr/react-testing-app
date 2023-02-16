6ae7a1a0c26ec9876e9c554f47c4e22cc05c74b8
kimhsr <kimhsr@naver.com>
2023-02-17 00:36:42 +0900

    useRef 공부3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import "./App.css";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
-  const [renderer, setRenderer] = useState(0);
-  const countRef = useRef(0);
-  let countVar = 0;
+  const [count, setCount] = useState(1);
+  const renderCount = useRef(1);
 
-  const doRendering = () => {
-    setRenderer(renderer + 1);
-  };
-
-  const increaseRef = () => {
-    countRef.current = countRef.current + 1;
-    console.log("ref: ", countRef.current);
-  };
-
-  const increaseVar = () => {
-    countVar = countVar + 1;
-    console.log("var: ", countVar);
-  };
-
-  const printResults = () => {
-    console.log(`ref: ${countRef.current}, var: ${countVar}`);
-  };
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+    console.log("렌더링 수: ", renderCount.current);
+  });
 
   return (
     <div>
-      <p>Ref: {countRef.current}</p>
-      <p>Var: {countVar}</p>
-      <button onClick={doRendering}>렌더!</button>
-      <button onClick={increaseRef}>Ref 올려</button>
-      <button onClick={increaseVar}>Var 올려</button>
-      <button onClick={printResults}>Ref Var 값 출력</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>올려</button>
     </div>
   );
 }
