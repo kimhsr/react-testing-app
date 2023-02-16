b5852c57f1af67173e64641dbcd463d17e543037
kimhsr <kimhsr@naver.com>
2023-02-16 22:44:39 +0900

    useRef 공부2

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@ import "./App.css";
 import React, { useState, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [renderer, setRenderer] = useState(0);
   const countRef = useRef(0);
+  let countVar = 0;
 
-  console.log("렌더링...");
-
-  const increaseCountState = () => {
-    setCount(count + 1);
+  const doRendering = () => {
+    setRenderer(renderer + 1);
   };
 
-  const increaseCountRef = () => {
+  const increaseRef = () => {
     countRef.current = countRef.current + 1;
-    console.log("Ref:", countRef.current);
+    console.log("ref: ", countRef.current);
+  };
+
+  const increaseVar = () => {
+    countVar = countVar + 1;
+    console.log("var: ", countVar);
+  };
+
+  const printResults = () => {
+    console.log(`ref: ${countRef.current}, var: ${countVar}`);
   };
 
   return (
     <div>
-      <p>State: {count}</p>
       <p>Ref: {countRef.current}</p>
-      <button onClick={increaseCountState}>State 올려</button>
-      <button onClick={increaseCountRef}>Ref 올려</button>
+      <p>Var: {countVar}</p>
+      <button onClick={doRendering}>렌더!</button>
+      <button onClick={increaseRef}>Ref 올려</button>
+      <button onClick={increaseVar}>Var 올려</button>
+      <button onClick={printResults}>Ref Var 값 출력</button>
     </div>
   );
 }
